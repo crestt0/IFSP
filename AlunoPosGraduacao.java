f79273a0435c80d25c2e51c45fd305dcf6e3f4da
vinicius <viniciuscrestane@gmail.com>
2024-10-29 10:44:38 -0300

    Create AlunoPosGraduacao.java

---
+++ AlunoPosGraduacao.java
@@ -0,0 +1,9 @@
+package aula2_java;
+
+public class AlunoPosGraduacao extends Aluno{
+	String areaPesquisa;
+	
+	void defenderTese() {
+		System.out.println("O aluno está defendendo a tese.");
+	}
+}
