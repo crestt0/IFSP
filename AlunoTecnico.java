1920a2df2b91f00007268670868f3ff6cbb271a8
vinicius <viniciuscrestane@gmail.com>
2024-11-12 13:05:02 -0300

    Delete AlunoTecnico.java

--- AlunoTecnico.java
+++
@@ -1,9 +0,0 @@
-package aula2_java;
-
-public class AlunoTecnico extends Aluno{
-	int cargaHoraria;
-	
-	void realizarProjeto() {
-		System.out.println("O aluno está realizando um projeto.");
-	}
-}
