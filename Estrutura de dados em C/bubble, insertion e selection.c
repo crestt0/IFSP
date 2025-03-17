4a142d8427eecbcbb31605166867725ba67d5f14
vinicius <viniciuscrestane@gmail.com>
2025-03-17 16:30:34 -0300

    Update bubble, insertion e selection.c

--- Estrutura de dados em C/bubble, insertion e selection.c
+++ Estrutura de dados em C/bubble, insertion e selection.c
@@ -1,18 +1,10 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    char buf[100];
-    int i, j, x;
+    char buf[100], aux[6];
+    int i, j, tam, x = 0;
     printf("insira a sequencia de numeros separados por virgula e termine com ponto (exemplo: 1, 2, 50, 0, 32.): ");
     gets(buf);
     for (i = 0, j = 0; buf[i] != '.'; i++){
@@ -20,20 +12,26 @@ int main()
             j++;
         }
     }
-    x = j + 1;
-    int vet[x];
+    tam = j + 1;
+    int vet[tam];
     
-    for (i = 0, j = 0; buf[i] != '.'; i++){
-        if (buf[i] == ','){
-            i++;
+    for (i = 0; buf[i] != '.'; i++){
+        if (buf[i] != ',' && buf[i] != ' '){
+            j = 0;
+            do {
+                aux[j] = buf[i];
+                i++;
+                j++;
+            } while (buf[i+1] != ',' || buf[i+1] != ',');
+            vet[x] = atoi(aux);
+            x++;
+            for (j = 0; j < 6; j++)
+                aux[j] = '.';
         }
-        else{
-            vet[j] = atoi(buf[i]);
-            j++;
-        }
+        
     }
     
-    for (i = 0; i < x; i++){
+    for (i = 0; i < tam; i++){
         printf("%i/ ", vet[i]);
     }
 }
