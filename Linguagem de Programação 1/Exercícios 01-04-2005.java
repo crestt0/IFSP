4ed5b9b7870d9f494b281f5b22a514ef4a0d695b
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-01 11:22:30 -0300

    Create Exercícios 01-04-2005.java

---
+++ Linguagem de Programação 1/Exercícios 01-04-2005.java
@@ -0,0 +1,91 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		
+		/*Scanner scanner = new Scanner (System.in);
+
+		float a = scanner.nextFloat();
+
+		float b = scanner.nextFloat();
+		
+		if (a == b) {
+			System.out.println("Iguais");
+		}
+		else {
+			System.out.println("Diferentes");
+		}
+		
+		Scanner scn = new Scanner(System.in);
+		
+		String SenhaCorreta = scn.nextLine();
+		String SenhaAtual = scn.nextLine();
+		
+		System.out.println(SenhaCorreta);
+		System.out.println(SenhaAtual);
+		
+		System.out.println((SenhaCorreta.equals(SenhaAtual)) ? "Acesso Concedido" : "Senha Incorreta");
+		
+		Scanner x = new Scanner(System.in);
+		
+		int Idade = x.nextInt();
+		
+		System.out.println((Idade > 18) ? "É maior que 18" : "Não é maior que 18");
+		
+		Scanner abc = new Scanner(System.in);
+		
+		Float Numero = abc.nextFloat();
+		
+		if (Numero == 0) {
+			System.out.println("Zero");
+		}
+		else {
+			System.out.println((Numero < 0) ? "Negativo" : "Positivo");
+		}
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int Pontos = scan.nextInt();
+		
+		System.out.println((Pontos >= 30) ? "Classificado para a próxima fase" : "Não classificado");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		float Temperatura = scan.nextFloat();
+		
+		System.out.println((Temperatura <= 0) ? "Possibilidade de neve" : "Sem previsão de neve");
+		
+		-------------------------------------------------------------------------------------
+		
+		Scanner scan = new Scanner(System.in);
+		
+		boolean TemCartaoLoja = scan.nextBoolean();
+		boolean CompraAcimaDeCemReais = scan.nextBoolean();
+		
+		System.out.println((TemCartaoLoja && CompraAcimaDeCemReais) ? "O cliente pode parcelar" : "O cliente não pode parcelar");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int Idade = scan.nextInt();
+		boolean AcompanhadaPorAdulto = scan.nextBoolean();
+		
+		System.out.println(((Idade >= 18) || AcompanhadaPorAdulto) ? "Permissão concedida" : "Permissão negada");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		boolean EmAvaliacaoGratuita = scan.nextBoolean();
+		
+		System.out.println((!EmAvaliacaoGratuita) ? "Acesso permitido ao conteúdo exclusivo" : "Acesso restrito durante o período de avaliação gratuita");
+		*/
+		
+		Scanner scan = new
+		int Idade
+		boolean AutorizacaoDosPais
+		float Altura
+	}
+
+}
+
