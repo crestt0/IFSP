4eddcfa6b4cc52e5ceb68094cc4fcf5428766a5c
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-14 11:29:34 -0300

    Update exercicio1.java

--- Linguagem de Programação 1/aula_2(java)-LPG1/exercicio1.java
+++ Linguagem de Programação 1/aula_2(java)-LPG1/exercicio1.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+import java.io.IOException;
 
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		String array[] = {"segunda feira", "terça feira", "quarta feira", "quinta feira", "sexta feira", "sabado", "domingo"};
+		
+		try {
+		    System.out.println("Insira o numero: ");
+		    int numero = scn.nextInt();
+		    System.out.println("O dia da semana é: " + array[numero-1]);
+		} 
+		catch (ArrayIndexOutOfBoundsException e) {
+		    System.out.println("Digite um numero de 1 a 7");
+		}
+		catch (Exception e) {
+		    System.out.println("Numero invalido");
+		}
+		
+		scn.close();
+	}
+}
