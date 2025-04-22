82bffb49110e999e2592880dc729457c339750cd
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-22 09:51:05 -0300

    Create exercicio_1.java

---
+++ Linguagem de Programação 1/aula_3(java)-LPG1/exercicio_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        List<String> lista = new ArrayList<>();
+        
+        lista.add ("Maçã");
+        lista.add ("Banana");
+        lista.add ("Pera");
+        lista.add ("Laranja");
+        
+        System.out.println(lista);
+    }
+}
