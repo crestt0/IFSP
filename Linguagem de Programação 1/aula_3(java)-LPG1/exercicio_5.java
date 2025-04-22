3b3a958ef7ff6d56355996fb196f8578ce2ae9b0
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-22 11:33:08 -0300

    Create exercicio_5.java

---
+++ Linguagem de Programação 1/aula_3(java)-LPG1/exercicio_5.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    
+        List<Integer> numeros = new ArrayList<>();
+        int[] vetor = {30, 10, 50, 20, 40};
+        int aux, indexMenor, i, j;
+        
+        for (int a : vetor)
+            numeros.add(a);
+        
+        //selection sort
+        for (i = 0; i < numeros.size(); i++){
+            indexMenor = i;
+            for (j = i; j < numeros.size(); j++){
+                if (numeros.get(j) < numeros.get(indexMenor)){
+                    indexMenor = j;
+                }
+            }
+            aux = numeros.get(indexMenor);
+            numeros.set(indexMenor, numeros.get(i));
+            numeros.set(i, aux);
+        }
+        
+        System.out.println(numeros);
+    }
+}
