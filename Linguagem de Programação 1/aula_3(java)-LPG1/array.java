8865d246808e1685a6c2b2b3ca7ed4f5e1bf0741
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-22 09:40:52 -0300

    Create array.java

---
+++ Linguagem de Programação 1/aula_3(java)-LPG1/array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        String lista[] = new String[4];
+        
+        lista[0] = "Maçã";
+        lista[1] = "Banana";
+        lista[2] = "Pera";
+        lista[3] = "Laranja";
+        
+        for (int i = 0; i < 4; i++){
+            System.out.printf("%s, ", lista[i]);
+        }
+    }
+}
