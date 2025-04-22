98dad2a8a52ab7728f6be44a7e32eeaf239461b3
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-22 10:12:47 -0300

    Create exercicio_3.java

---
+++ Linguagem de Programação 1/aula_3(java)-LPG1/exercicio_3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        List<String> lista = new ArrayList<>();
+        
+        lista.add ("Maçã");
+        lista.add ("Banana");
+        lista.add ("Pera");
+        lista.add ("Laranja");
+        
+        //int PosicaoBanana = lista.indexOf("Banana");
+        //lista.set(PosicaoBanana, "Abacaxi");
+        
+        lista.set(lista.indexOf("Banana"), "Abacaxi");
+        
+        System.out.println(lista);
+    }
+}
