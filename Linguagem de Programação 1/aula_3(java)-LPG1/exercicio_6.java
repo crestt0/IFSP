9677bd0ae310e6fa7a4436a2f8fed2a403b9d003
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-04-24 09:54:11 -0300

    Create exercicio_6.java

---
+++ Linguagem de Programação 1/aula_3(java)-LPG1/exercicio_6.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    
+        List<Integer> numeros = new ArrayList<>();
+        int[] vetor = {30, 10, 50, 20, 40};
+        int aux, indexMenor, i, j;
+        
+        for (int a : vetor)
+            numeros.add(a);
+        
+        //selection sort
+        for (i = 0; i < numeros.size(); i++){
+            indexMenor = i;
+            for (j = i; j < numeros.size(); j++){
+                if (numeros.get(j) < numeros.get(indexMenor)){
+                    indexMenor = j;
+                }
+            }
+            aux = numeros.get(indexMenor);
+            numeros.set(indexMenor, numeros.get(i));
+            numeros.set(i, aux);
+        }
+        
+        Collections.sort(numeros);
+        
+        if (numeros.contains(50)){
+            numeros.remove((Integer) 50);
+            System.out.println("Numero 50 removido");
+        }
+        
+        Collections.shuffle(numeros);
+        
+        System.out.println(numeros);
+    }
+}
