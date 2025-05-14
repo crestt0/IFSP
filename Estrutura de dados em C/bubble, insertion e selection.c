bae9e1e3d84871cfc3100bee204f93e98e6822ba
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-05-14 11:02:59 -0300

    Update bubble, insertion e selection.c

--- Estrutura de dados em C/bubble, insertion e selection.c
+++ Estrutura de dados em C/bubble, insertion e selection.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void bubble(int *vet, int tam){
+    int i, j, aux;
+    
+    for (i = 0; i < tam; i++){
+        for(j = 0; j < tam - i; j++){
+            if (vet[j] > vet[j+1]){
+                aux = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
+            }
+        }
+    }
+}
+
+void selection(int *vet, int tam){
+    int i, j, aux, indexMenor;
+    
+    for (i = 0; i < tam; i++){
+        indexMenor = i;
+        for (j = i; j < tam; j++){
+            if(vet[j] < vet[indexMenor]){
+                indexMenor = j;
+            }
+        }
+        aux = vet[indexMenor];
+        vet[indexMenor] = vet[i];
+        vet[i] = aux;
+    }
+}
+
+void insertion(int *vet, int tam){
+    int i, j, aux;
+    
+    for (i = 0; i < tam; i++){
+        for (j = i; ){
+            
+        }
+    }
+}selectio
+
 int main()
 {
-    int buffer[1000], tam = 0;
+    int buffer[1000], tam = 0, i;
     
     printf("entre com o valor dos numeros separado por espaço e termine com 'f'(exemplo: 1 3 50 2 4444 0 f): ");
     
@@ -17,6 +57,8 @@ int main()
     for (i = 0; i < tam; i++) {
         vetor[i] = buffer[i];
     }
+    
+    insertion(vetor, tam);
     
     for (i = 0; i < tam; i++){
         printf("%i /", vetor[i]);
