14f4c2cc4af9f6c0251455f4d4c5079647fcf356
Vinicius Crestane <viniciuscrestane@gmail.com>
2025-05-14 15:40:49 -0300

    Update bubble, insertion e selection.c

--- Estrutura de dados em C/bubble, insertion e selection.c
+++ Estrutura de dados em C/bubble, insertion e selection.c
@@ -5,11 +5,11 @@ void bubble(int *vet, int tam){
     int i, j, aux;
     
     for (i = 0; i < tam; i++){
-        for(j = 0; j < tam - i; j++){
-            if (vet[j] > vet[j+1]){
+        for(j = 0; j < tam - i - 1; j++){
+            if (vet[j] > vet[j + 1]){
                 aux = vet[j];
-                vet[j] = vet[j+1];
-                vet[j+1] = aux;
+                vet[j] = vet[j + 1];
+                vet[j + 1] = aux;
             }
         }
     }
@@ -34,35 +34,73 @@ void selection(int *vet, int tam){
 void insertion(int *vet, int tam){
     int i, j, aux;
     
-    for (i = 0; i < tam; i++){
-        for (j = i; ){
-            
+    for (i = 1; i < tam; i++){
+        for (j = i; vet[j - 1] > vet[j] && j != 0; j--){
+            aux = vet[j - 1];
+            vet[j - 1] = vet[j];
+            vet[j] = aux;
         }
     }
-}selectio
+}
 
 int main()
 {
-    int buffer[1000], tam = 0, i;
+    int buffer[1000], tam = 0, i, *vetor;
+    char resposta;
     
-    printf("entre com o valor dos numeros separado por espaço e termine com 'f'(exemplo: 1 3 50 2 4444 0 f): ");
-    
-    int result;
-    while ((result = scanf("%i", &buffer[tam])) == 1) {
-        tam++;
+    while(1){
+        printf("\nfuncionalidades: \n\ninserir elementos na lista(1)\nvisualizar a lista atual(2)\napagar a lista atual(3)\nordenar por bubble sort(4)\nordenar por selection sort(5)\nordenar por insertion sort(6)\n\nresposta: ");
+        scanf(" %c", &resposta);
+        switch(resposta){
+        case '1':
+            printf("entre com o valor dos numeros separado por espaço e termine com 'f'(exemplo: 1 3 50 2 4444 0 f): ");
+            
+            int result;
+            while ((result = scanf("%i", &buffer[tam])) == 1) {
+                tam++;
+            }
+            
+            vetor = malloc(tam * sizeof(int));
+            
+            for (i = 0; i < tam; i++) {
+                vetor[i] = buffer[i];
+            }
+            printf("\n\n");
+            break;
+        case '2': 
+            if(tam == 0){
+                printf("\nnenhum número foi inserido ainda\n");
+            }
+            else{
+                printf("\nvetor atual: ");
+                for (i = 0; i < tam; i++){
+                    printf("%i, ", vetor[i]);
+                }
+                printf("\n\n");
+            }
+            break;
+        case '3':
+            vetor = realloc(vetor, sizeof(int));
+            tam = 0;
+            printf("\nlista apagada com sucesso\n");
+            break;
+        case '4':
+            bubble(vetor, tam);
+            printf("\nvetor ordenado pelo bubble sort\n");
+            break;
+        case '5':
+            selection(vetor, tam);
+            printf("\nvetor ordenado pelo selection sort\n");
+            break;
+        case '6':
+            insertion(vetor, tam);
+            printf("\nvetor ordenado pelo insertion sort\n");
+            break;
+        default:
+            printf("\ncomando não identificado\n");
+            break;
+        }
     }
 
-    int vetor[tam];
-
-    for (i = 0; i < tam; i++) {
-        vetor[i] = buffer[i];
-    }
-    
-    insertion(vetor, tam);
-    
-    for (i = 0; i < tam; i++){
-        printf("%i /", vetor[i]);
-    }
-    
     return 0;
 }
